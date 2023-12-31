7e7dbe0960f9a4813e5826340e6336c1f06c0e07
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-12-31 16:35:45 +0100

    final commit for variables

--- MainClass.java
+++ MainClass.java
@@ -1,19 +1,19 @@
 public class MainClass {
     public static void main(String[] args) {
-        // int firstSmallInt = 10;
-        // long firstLongInt = 1000000000;
+        int firstSmallInt = 10;
+        long firstLongInt = 1000000000;
 
-        // float firstFloat = 3.4f;
-        // double firstDouble = 3.4;
+        float firstFloat = 3.4f;
+        double firstDouble = 3.4;
 
-        // char firstChar = 'O';
-        // String firstString = "I am O";
+        char firstChar = 'O';
+        String firstString = "I am O";
 
-        // boolean firstTrue = true;
-        // boolean firstFalse = false;
+        boolean firstTrue = true;
+        boolean firstFalse = false;
 
-        // int[] firstIntArray = {1, 2, 3, 4, 5};
-        // String[] firstStringArray = {"Java", "is", "Strong"};
+        int[] firstIntArray = {1, 2, 3, 4, 5};
+        String[] firstStringArray = {"Java", "is", "Strong"};
 
         class User {
             String userName;
