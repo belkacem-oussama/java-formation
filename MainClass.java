a2ded6adf5744b6d661232aeba397d0301ef9c75
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-12-31 18:06:16 +0100

    algo if...else

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,15 @@
 public class MainClass {
     public static void main(String[] args) {
-       System.out.println("Hello World !");
+        int firstInt = 89;
+        int secondInt = 100;
+        String firstString = "Match !";
+
+        if ((firstInt + secondInt) == 189 ) {
+            System.out.println(firstString);
+        } else {
+            System.out.println("No match ...");
+        }
     }
 }
+
+//Output => "Match !"
