9b89f4ab942c88094b1f78f8ce419164f6b23cf1
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-12-31 18:22:37 +0100

    algo switch case

--- MainClass.java
+++ MainClass.java
@@ -2,9 +2,13 @@ public class MainClass {
     public static void main(String[] args) {
         int firstInt = 80;
 
-        String result = (firstInt == 80) ? "First int is 80. " : "Not 80.";
-
-        System.out.println(result);
+        switch (firstInt) {
+            case 80: System.out.println("First int is 80.");
+                break;
+            case 90: System.out.println("First int is 90.");
+            default:
+                break;
+        }
     }
 }
 
